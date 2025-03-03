b5e3240d08e00947cbc4f9d01c7212723d3d9fcc
Kapza <lukakapetanovic@icloud.com>
2025-03-03 19:19:37 +0100

    Changed starborder component padding.

--- src/pages/starborder.tsx
+++ src/pages/starborder.tsx
@@ -37,7 +37,7 @@ const StarBorder = <T extends React.ElementType = "button">({
           animationDuration: speed,
         }}
       ></div>
-      <div className="relative z-1 bg-gradient-to-b from-black to-zinc-900 border border-zinc-900  text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
+      <div className="relative z-1 bg-gradient-to-b from-black to-zinc-900 border border-zinc-900  text-white text-center text-[16px] py-[10px] px-[20px] mb-1 rounded-[20px]">
         {children}
       </div>
     </Component>
