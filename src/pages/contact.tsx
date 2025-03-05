324cfe6ddf7c53676ea48b5d534d1abba635c33a
Kapza <lukakapetanovic@icloud.com>
2025-03-05 18:24:10 +0100

    added emailJS

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./nav";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  //  if route is contact then Nav shoudl be mt-4 mr-4 ml-4
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSent, setIsSent] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //EmailJS service ID, template ID, and public Key
+    const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+    if (!serviceId || !templateId || !publicKey) {
+      console.error("EmailJS service ID, template ID, or public key is missing.");
+      return;
+    }
+
+    //create a new objec that contains template parameters
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "Luka Kapetanovic",
+      message: message,
+    };
+    //send the email using the emailJS send method
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log("Email was sent successfully", response);
+        setIsSent(true);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.log("Email was not sent", error);
+      });
+  };
 
   return (
     <>
@@ -118,25 +157,83 @@ const Contact = () => {
               <div className="top-contact-logo absolute custom-orange-font">O</div>
               <div className="bottom-contact-logo absolute custom-orange-font">O</div>
               <div className="contact-us-form relative rounded-lg bg-zinc-900 p-8 shadow-lg dark:bg-dark-2 sm:p-12">
-                <form>
-                  <ContactInputBox type="text" name="name" placeholder="Your Name" />
-                  <ContactInputBox type="text" name="email" placeholder="Your Email" />
-                  <ContactInputBox type="text" name="phone" placeholder="Your Phone" />
-                  <ContactTextArea
-                    row={6}
-                    placeholder="Your Message"
-                    name="details"
-                    defaultValue=""
-                  />
-                  <div>
-                    <button
-                      type="submit"
-                      className=" contact-us-send-btn w-full rounded border border-primary p-3 bg-zinc-900"
-                    >
-                      Send Message
-                    </button>
+                <div className="wrapper">
+                  <div className="thank-you-msg">
+                    {isSent && (
+                      <div
+                        className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+                        role="alert"
+                      >
+                        <strong className="font-bold">Thank you for contacting us!</strong>
+                        <span className="block sm:inline">
+                          We will get back to you as soon as possible.
+                        </span>
+                      </div>
+                    )}
                   </div>
-                </form>
+                  <form onSubmit={handleSubmit} className="w-full max-w-lg">
+                    <div className="mb-4">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                        Name
+                      </label>
+                      <div className="relative">
+                        <input
+                          type="text"
+                          placeholder="Full name "
+                          id="name"
+                          name="name"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                          className="input-field bg-gray-100 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="mb-4">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Email
+                      </label>
+                      <div className="relative">
+                        <input
+                          type="email"
+                          id="email"
+                          placeholder="Your email here..."
+                          name="email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          className=" input-field bg-gray-100 shadow appearance-none border rounded w-full py-2  text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="message"
+                      >
+                        Message
+                      </label>
+                      <textarea
+                        id="message"
+                        placeholder="Your message here..."
+                        name="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className="input-field-msg bg-gray-100 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        required
+                      />
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <button
+                        id="submit-button"
+                        type="submit"
+                        className="dark-mint hover:dark-mint-btn-hvr text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      >
+                        Send Message
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
@@ -147,47 +244,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-interface ContactTextAreaProps {
-  row: number;
-  placeholder: string;
-  name: string;
-  defaultValue: string;
-}
-
-const ContactTextArea = ({ row, placeholder, name, defaultValue }: ContactTextAreaProps) => {
-  return (
-    <>
-      <div className="mb-6">
-        <textarea
-          rows={row}
-          placeholder={placeholder}
-          name={name}
-          className="w-full resize-none rounded border border-stroke px-[14px] py-3 text-base bg-zinc-900 text-white"
-          defaultValue={defaultValue}
-        />
-      </div>
-    </>
-  );
-};
-
-interface ContactInputBoxProps {
-  type: string;
-  placeholder: string;
-  name: string;
-}
-
-const ContactInputBox = ({ type, placeholder, name }: ContactInputBoxProps) => {
-  return (
-    <>
-      <div className="mb-6">
-        <input
-          type={type}
-          placeholder={placeholder}
-          name={name}
-          className="w-full rounded border border-stroke px-[14px] py-3 text-base bg-zinc-900 text-white"
-        />
-      </div>
-    </>
-  );
-};
